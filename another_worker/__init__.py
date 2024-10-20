c767dd807b813a3ea924d4e376d0e460390d581e
Kamil <kami.krasowski.uk@gmail.com>
2024-10-20 21:21:56 +0100

    override wsgi module

--- another_worker/__init__.py
+++ another_worker/__init__.py
@@ -1,9 +1,5 @@
 from quart import Quart
-import asyncio
-from hypercorn.config import Config
-from hypercorn.asyncio import serve
 
 app = Quart(__name__)
-quart_app = asyncio.run(serve(app, Config()))
 
 import another_worker.routes
--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,8 +1,11 @@
 import sys
 import os
+import asyncio
+from hypercorn.config import Config
+from hypercorn.asyncio import serve
 
 sys.path.append(os.getcwd())
 
-from another_worker import quart_app as application
+from another_worker import app
 
-
+application = asyncio.run(serve(app, Config(), mode="asgi"))
